d7d9e9eead6f3cb6ae02659aa00720d5492423e1
Priyankajeev <priyankajeev2359@gmail.com>
2025-06-30 15:24:30 +0530

    #program.cs comitted

--- WebApplicationClinicalManagementSystem/Program.cs
+++ WebApplicationClinicalManagementSystem/Program.cs
@@ -28,7 +28,7 @@ namespace WebApplicationClinicalManagementSystem
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Login}/{id?}");
 
             app.Run();
         }
