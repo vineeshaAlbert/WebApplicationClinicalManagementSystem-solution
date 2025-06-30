c28745d39a232aa1bc9b205074e4fea3c75751d4
91915 <91915@VINEESHAPC>
2025-06-30 14:47:36 +0530

    Add project files.

---
+++ WebApplicationClinicalManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationClinicalManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
